eb3ef0996e7478d5fc9a5cd8467c9fe06b5989d1
Obi <103497527+OhDG@users.noreply.github.com>
2025-05-01 14:09:09 +0900

    Revert "mission/#5"
    This reverts commit d730e6f0b8801b7917bb56b033f5683543f64439.

---
+++ spring/src/main/java/umc/spring/Application.java
@@ -0,0 +1,27 @@
+package umc.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import umc.spring.repository.CoffeeRepository;
+import umc.spring.entity.Coffee;
+
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner testDbConnection(CoffeeRepository coffeeRepository) {
+		return args -> {
+
+			coffeeRepository.save(new Coffee("latte", "라떼"));
+
+			System.out.println("☕️ 커피 개수: " + coffeeRepository.count());
+		};
+	}
+}
