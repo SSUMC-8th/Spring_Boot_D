a9342a5a82104a38b016b6b8fd521f70d5bfd922
Obi <103497527+OhDG@users.noreply.github.com>
2025-05-01 14:09:09 +0900

    Revert "practice/#04"
    This reverts commit 7bac43634dbefd9927b8d7a8a681f26eb83b0b59.

--- spring/src/main/java/umc/spring/repository/CoffeeRepository.java
+++
@@ -1,7 +0,0 @@
-package umc.spring.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import umc.spring.entity.Coffee;
-
-public interface CoffeeRepository extends CrudRepository<Coffee, String> {
-}
