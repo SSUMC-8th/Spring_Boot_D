a4d37c5cc64f98afc5e2e8880bd68972810d5c8f
qkrcodus <qkrcodus0722@gmail.com>
2025-05-07 15:42:05 +0900

    mnu/#6 checkout 을 위한 커밋

---
+++ spring/src/main/generated/umc/spring/domain/QReview.java
@@ -0,0 +1,52 @@
+package umc.spring.domain;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QReview is a Querydsl query type for Review
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QReview extends EntityPathBase<Review> {
+
+    private static final long serialVersionUID = 1510377220L;
+
+    public static final QReview review = new QReview("review");
+
+    public final umc.spring.domain.common.QBase _super = new umc.spring.domain.common.QBase(this);
+
+    public final StringPath content = createString("content");
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> createdAt = _super.createdAt;
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final ListPath<ReviewImage, QReviewImage> reviewImages = this.<ReviewImage, QReviewImage>createList("reviewImages", ReviewImage.class, QReviewImage.class, PathInits.DIRECT2);
+
+    public final NumberPath<Integer> star = createNumber("star", Integer.class);
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> updatedAt = _super.updatedAt;
+
+    public QReview(String variable) {
+        super(Review.class, forVariable(variable));
+    }
+
+    public QReview(Path<? extends Review> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QReview(PathMetadata metadata) {
+        super(Review.class, metadata);
+    }
+
+}
+
