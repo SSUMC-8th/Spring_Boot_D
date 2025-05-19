78a5378857fee3104a00b31a55920edb796afdd8
OhDG <ohdonggyu@naver.com>
2025-05-19 17:40:54 +0900

    mission/#8-1
    회원가입 API

--- src/main/java/umc/spring/domain/mapping/FavoriteFood.java
+++ src/main/java/umc/spring/domain/mapping/FavoriteFood.java
@@ -23,4 +23,16 @@ public class FavoriteFood extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "food_id")
     private Food food;
+
+    public void setMember(Member member) {
+        if (this.member != null) {
+            member.getFavoriteFoodList().remove(this);
+        }
+        this.member = member;
+        member.getFavoriteFoodList().add(this);
+    }
+
+    public void setFood(Food food) {
+        this.food = food;
+    }
 }
