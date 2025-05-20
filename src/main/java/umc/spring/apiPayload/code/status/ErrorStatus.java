d7c9772fed0f5e5da6b0b8e349d89708f3d34b4f
OhDG <ohdonggyu@naver.com>
2025-05-20 20:38:30 +0900

    mission/#8-3
    리뷰추가 API

--- src/main/java/umc/spring/apiPayload/code/status/ErrorStatus.java
+++ src/main/java/umc/spring/apiPayload/code/status/ErrorStatus.java
@@ -24,7 +24,9 @@ public enum ErrorStatus implements BaseErrorCode {
 
     FOOD_CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND, "FOOD4001", "음식 카테고리가 없습니다."),
 
-    REGION_NOT_FOUND(HttpStatus.NOT_FOUND, "REGION40001", "지역이 존재하지 않습니다.");
+    REGION_NOT_FOUND(HttpStatus.NOT_FOUND, "REGION40001", "지역이 존재하지 않습니다."),
+
+    RESTAURANT_NOT_FOUND(HttpStatus.NOT_FOUND, "RESTAURANT4001", "식당이 존재하지 않습니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
---
+++ src/main/java/umc/spring/web/dto/ReviewRequestDTO.java
@@ -0,0 +1,22 @@
+package umc.spring.web.dto;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+
+import java.util.List;
+
+public class ReviewRequestDTO {
+
+    @Getter
+    public static class JoinDTO {
+        @NotBlank
+        String text;
+
+        @Min(1) @Max(5)
+        Float score;
+
+        List<String> reviewPictureList;
+    }
+}
